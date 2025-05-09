8feb3aaa1e79628b87c7bd17869c8d36660661ed
auouo12345 <auouo12345@gmail.com>
2025-05-09 15:39:46 +0800

    Title effect

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,11 +1,30 @@
-import Background from './assets/LoginPage_Background2.jpg'
-import './LoginPage.css'
+import Background from './assets/LoginPage_Background2.jpg';
+import './LoginPage.css';
 import LoginForm from "./components/LoginForm.jsx";
-import {useState} from "react";
+import { useEffect, useState } from "react";
 
 
 function LoginPage({onLogin , setToken}) {
-    const [isStart , setStart] = useState(false)
+
+    const [titleIndex , setTitleIndex] = useState(0);
+    const [fade , setFade] = useState(true)
+    const [isStart , setStart] = useState(false);
+    const content = ["一眼掌握你的健康狀況" , "讓健康數據誠為日常習慣" , "簡潔呈現，深刻理解" , "數據為你說話" , "健康不該複雜"];
+    const contentEn =["Health insights, clearly delivered" , "Your health, visualized and simplified" , "Simplicity meets meaning health data" , "Where numbers tell your health story" , "Clear. Concise. Connected."]
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            // 先淡出
+            setFade(false);
+            setTimeout(() => {
+                // 換字後淡入
+                setTitleIndex((prev) => (prev + 1) % content.length);
+                setFade(true);
+            }, 1000); // 這裡要小於 interval 的間隔時間
+        }, 3000); // 每 3 秒切換一次
+
+        return () => clearInterval(interval);
+    }, []);
 
     return (
         <div
@@ -27,8 +46,8 @@ function LoginPage({onLogin , setToken}) {
             </div>
             {isStart ? <LoginForm onLogin={onLogin} setToken={setToken}></LoginForm> :
                 <div className="flex flex-col items-center">
-                    <p className="text-white text-5xl font-bold text-center">一眼掌握你的健康狀況</p>
-                    <p className="text-white text-5xl font-bold text-center">Health insights, clearly delivered</p>
+                    <p className={`text-white text-5xl font-bold text-center transition-opacity duration-1000 ${fade ? "opacity-100" : "opacity-0"}`}>{content[titleIndex]}</p>
+                    <p className={`text-white text-5xl font-bold text-center transition-opacity duration-1000 ${fade ? "opacity-100" : "opacity-0"}`}>{contentEn[titleIndex]}</p>
                     <button
                         className="mt-15 rounded-lg bg-gradient-to-br from-purple-600 to-blue-500 py-2 text-sm font-medium text-white w-[120px] hover:bg-gradient-to-bl focus:outline-none focus:ring-4 focus:ring-blue-300"
                         onClick={() => {
