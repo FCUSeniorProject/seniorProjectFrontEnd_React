85ebacfeebf6135342c6ac782ac731a94bbabc2f
nameless0629 <sob9987@gmail.com>
2025-05-14 05:06:02 +0800

    Update Sidebar

--- src/components/SidebarItem.jsx
+++ src/components/SidebarItem.jsx
@@ -6,9 +6,9 @@ function SidebarItem({select , setSelect , title , contents}) {
 
     return (
         <div className="mb-7">
-            <div className="flex h-auto items-center border-black pl-2 border-b-1 w-[100%]">
+            <div className="flex h-auto items-center border-black pl-2 border-b-1 w-[100%]" onClick={() => toggleOpen(!isOpen)}>
                 {isOpen ? <SlArrowDown color="#FFFFFF" size="20"/> : <SlArrowRight color="#FFFFFF" size="20"/>}
-                <p className="ml-2 cursor-pointer text-3xl font-bold text-white" onClick={() => toggleOpen(!isOpen)}>{title}</p>
+                <p className="ml-2 cursor-pointer text-3xl font-bold text-white" >{title}</p>
             </div>
             {isOpen && (
                 <ul className="mb-3 border-black pb-3 border-b-1 w-[100%]">
